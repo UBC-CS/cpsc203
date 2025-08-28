4bc372ea00fed53fb0019e584a75138a5ebd1236
Stephan Koenig <stephan.koenig@gmail.com>
2025-08-28 14:45:19 -0700

    Reorganize schedule

--- R/combine-pdfs.R
+++ R/combine-pdfs.R
@@ -1,7 +1,7 @@
 library(fs)
 library(qpdf)
 
-worksheets <- path("_site", "activities", "01_analysis-review.pdf")
+worksheets <- path("_site", "activities", "tbd_tbd_analysis-review.pdf")
 output <- path("_site", "activities", "analysis.pdf")
 combined_pdf <- file_temp(ext = "pdf")
 combined_pdf <- pdf_combine(c(worksheets, output), combined_pdf)
